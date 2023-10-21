acd9f13b86b7799a818dcd78250e37fe768a8e9b
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2023-10-21 23:59:36 +0530

    #include <stdio.h> int main() {     int  x,y;     scanf("%d",&x);     if(x%10==0)     {         y=x/10;         printf("%d",y);     }     else if(x%5==0 && x%10!=0)     {       y=(x/10)+1;         printf("%d",y) ;     }     else     {         printf("-1");     }
    }

---
+++ Kohli_and_Coins.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int  x,y;
+    scanf("%d",&x);
+    if(x%10==0)
+    {
+        y=x/10;
+        printf("%d",y);
+    }
+    else if(x%5==0 && x%10!=0)
+    {
+      y=(x/10)+1;
+        printf("%d",y) ;
+    }
+    else
+    {
+        printf("-1");
+    }
+    
+}
