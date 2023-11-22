51018a6b22e692e4632cb94565eb98c9de476a63
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2023-11-22 16:28:07 +0530

    #include<stdio.h>
     int main (){
         int n;
         scanf("%d",&n);
         
         for ( int i = 1; i <= n; i++){
             for ( int j = 65; j <=64+ n; j++){
                 printf("%c ",  j);
             }   printf("
    ");
         }
    
     }

---
+++ Basic_Pattern_1.4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+ int main (){
+     int n;
+     scanf("%d",&n);
+     
+     for ( int i = 1; i <= n; i++){
+         for ( int j = 65; j <=64+ n; j++){
+             printf("%c ",  j);
+         }   printf("
+");
+     }
+
+ }
