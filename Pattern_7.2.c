c581de5aab83317b6ba213dd076c77b399c4e360
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2023-12-05 13:16:38 +0530

    #include <stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=0;j<n-i;j++)         {             printf(" ");         }         for(k=i;k>0;k--)         {             printf("%d ",i);         }printf(" ");     } }

---
+++ Pattern_7.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=0;j<n-i;j++)
+        {
+            printf(" ");
+        }
+        for(k=i;k>0;k--)
+        {
+            printf("%d ",i);
+        }printf("
+");
+    }
+}
