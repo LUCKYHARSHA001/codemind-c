3f6d50ed5c8fa346096d3a41994e07e3ea380abd
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2023-12-27 10:39:17 +0530

    #include <stdio.h> #include <ctype.h> int main() {     char str[100];     scanf("%[^ ]s",str);     int i;     for(i=0;str[i]!=NULL;i++)     {       str[i]=tolower(str[i]);       printf("%c",str[i]);     }
    }

---
+++ To_Lower_Case.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+int main()
+{
+    char str[100];
+    scanf("%[^
+]s",str);
+    int i;
+    for(i=0;str[i]!=NULL;i++)
+    {
+      str[i]=tolower(str[i]);
+      printf("%c",str[i]);
+    }
+    
+}
