f305412dce8eb38ca468da4e4ef26914dfcb7ebb
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2023-12-31 15:23:49 +0530

    #include <stdio.h> int main() {     char str[1000];     int count=0,i;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]=='a'|| str[i]=='e'|| str[i]=='i'||str[i]=='o'||str[i]=='u')         {             count++;         }     }     printf("%d",count);
    }

---
+++ Vowels_Count.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char str[1000];
+    int count=0,i;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]=='a'|| str[i]=='e'|| str[i]=='i'||str[i]=='o'||str[i]=='u')
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+    
+}
