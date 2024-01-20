c7819462312bdd73f444db58c8f65258ba52e122
LUCKYHARSHA001 <147419131+LUCKYHARSHA001@users.noreply.github.com>
2024-01-21 00:03:18 +0530

    #include<iostream> using namespace std; int main() {     int n;     cin>>n;     for(int i=1;i<=n;i++)     {         if(n%i==0)         {             cout<<i<<' ';         }     } }

--- Find_Factors.c
+++ Find_Factors.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-    int n,i;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
     {
         if(n%i==0)
         {
-            printf("%d ",i);
+            cout<<i<<' ';
         }
     }
-    
 }
